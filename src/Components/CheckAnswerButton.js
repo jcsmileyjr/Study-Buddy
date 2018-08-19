9a79d779c7143f11990c875fafca6a3b59798c39
JC Smiley <jcsmileyjr@gmail.com>
2018-08-18 20:00:47 -0500

    Update test's passFail with a Redux action
    - Create a method to change the passfail atribute of the tests objects
    based on if its the correct answer
    - The method is called in the CheckAnswerButton.
    - It use a action to pass an updated array of answers to the Redux State

--- src/Components/CheckAnswerButton.js
+++ src/Components/CheckAnswerButton.js
@@ -3,6 +3,7 @@ import {Button} from 'react-bootstrap';
 import { StyleSheet, css } from 'aphrodite';
 import {connect} from 'react-redux';
 import {addCorrectAnswer} from '../Actions/correctCountActions.js';
+import {updatePassFail} from '../Actions/testActions.js';
 
 const styles = StyleSheet.create({
   whiteSpaceAboveElement:{
@@ -25,6 +26,21 @@ const styles = StyleSheet.create({
 });
 
 class CheckAnswerButton extends Component{
+	
+  //method to change the passFail attribute of each test's object to pass or fail. This will be use to change the CSS to green for correct and red for incorrect. 	
+  showCorrectAnswer(){
+	  let newTest = this.props.currentTest;
+	  newTest.forEach((test) =>{
+		//if the current test answer is the correct answer, change to pass. Else, change to fail.  
+	    if(this.props.currentAnswer.userAnswer === test.answer){
+			test.passFail = "pass";
+		}else {
+			test.passFail = "fail";
+		}
+	  });
+	  console.dir(newTest);
+	  this.props.onUpdatePassFail(newTest);
+  }	
 
   //Method use when user click the button. It adds one to the  correct answer count if the user selected answer is correct
   onCheckAnswer = event => {
@@ -32,7 +48,7 @@ class CheckAnswerButton extends Component{
 	  if(this.props.currentAnswer.userAnswer === this.props.currentTest[this.props.questionsAnswered.questionAnswered].answer){
 		  this.props.onAddCorrectAnswer(); //If the condition above is true, add one to the current count of correctly answered questions  
 	  }	
-
+	  this.showCorrectAnswer();//change the passFail attribute update the CSS in the answers
   }	
 
   render(){
@@ -61,7 +77,8 @@ const mapStateToProps = state =>({
 
 //map the imported Redux actions to a local method to be used by the component. This will allow the components to change the state of the Redux store
 const mapActionsToProps = {
-  onAddCorrectAnswer: addCorrectAnswer
+  onAddCorrectAnswer: addCorrectAnswer,
+  onUpdatePassFail: updatePassFail
 };
 
 export default connect(mapStateToProps,mapActionsToProps)(CheckAnswerButton);
