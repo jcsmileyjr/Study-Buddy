f20f250933e585fab10f099f2aa3a668c0804f37
JC Smiley <jcsmileyjr@gmail.com>
2018-08-31 15:26:04 -0500

    Change SuccessPage into a es6 Class
    - Wanted to create a method to use with onClick. So change the regular component into a class

--- src/Components/SuccessPage.js
+++ src/Components/SuccessPage.js
@@ -6,28 +6,29 @@ import {Button} from 'react-bootstrap';
 import {leaveSuccessPage} from '../Actions/hideSuccesPageAction.js';//import action to change the SuccessPage state's showSuccessPage state to false therefore hiding it.
 
 //A component shown when the user completes a quiz. A button is press to preceed to the next quiz
-function SuccessPage({currentScore}){
+class SuccessPage extends Component{
   
-  if(currentScore.score > 75){
+  if(this.props.currentScore.score > 75){
     const currentMessage = "You Passed, Proceed to the next next level";
   }else{
     const currentMessage = "You Failed, let's try again";  
   }
     
-  const onCloseSuccessPage = () => {
+  onCloseSuccessPage = () => {
     this.props.onClosePage();  
   }      
-     
-  return(
-    <div className="row">
-	  <div className="col-xs-12 text-center">
-        <h1>{currentMessage}</h1>
-	  </div>
-	  <div className="col-xs-12 text-center">
-        <Button onClick={onCloseSuccessPage} bsStyle="success">Done</Button>        
-	  </div>
-	</div>  
-  );	
+  render(){  
+      return(
+        <div className="row">
+          <div className="col-xs-12 text-center">
+            <h1>{currentMessage}</h1>
+          </div>
+          <div className="col-xs-12 text-center">
+            <Button onClick={this.onCloseSuccessPage} bsStyle="success">Done</Button>        
+          </div>
+        </div>  
+      );	
+  }
 }
 
 /*Use Redux to get the current score from state*/
@@ -39,4 +40,4 @@ const mapActionsToProps = {
     onClosePage: leaveSuccessPage
 };
 
-export default connect(mapStateToProps)(Nav);
+export default connect(mapStateToProps, mapActionsToProps)(SuccessPage);
