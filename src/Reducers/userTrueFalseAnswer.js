46891f35076ffe85a164bf0cd30ec67fc9d9d568
JC Smiley <jcsmileyjr@gmail.com>
2018-09-11 16:16:55 -0500

    Bug: TrueFalseAnswer state not updating
    - The issue is the actions to the state had different names for the
    state. This cause the to delete itself.
    
    - Updated the actions to use the same name.

--- src/Reducers/userTrueFalseAnswer.js
+++ src/Reducers/userTrueFalseAnswer.js
@@ -3,17 +3,17 @@ export default function currentUserTrueFalseAnswer(state={"truefalse": false, "t
 	switch(type){
 		case 'USERANSWERTRUE':
 			return {
-				truthFalse: true, //Update the state to true 
+				truefalse: true, //Update the state to true 
                 trueFalseQuizAnswer: state.trueFalseQuizAnswer
 			};
 		case 'USERANSWERFALSE':
 			return {
-				truthFalse: false, //Reset the state to false 
+				truefalse: false, //Reset the state to false 
                 trueFalseQuizAnswer: state.trueFalseQuizAnswer
 			};
 		case 'UPDATETRUEFALSEQUIZANSWER':
 			return {
-				truthFalse: state.truefalse, //Reset the state to false 
+				truefalse: state.truefalse, //Reset the state to false 
                 trueFalseQuizAnswer: payload
 			};		            
 		default:
