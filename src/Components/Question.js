74c67ecdff2288c6aa09d4236474a3cbc35acda2
JC Smiley <jcsmileyjr@gmail.com>
2018-10-06 15:40:16 -0500

    Inital Modal showing title of List of Answers
    - transform Question function into a class
    - add React Bootstrap Modal functionality

--- src/Components/Question.js
+++ src/Components/Question.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Modal, Button} from 'react-bootstrap';
 import { StyleSheet, css } from 'aphrodite';
 import {connect} from 'react-redux';
 
@@ -12,29 +13,57 @@ const styles = StyleSheet.create({
 });
 
 //An permanent component that displays the current question from the test state. A book icon is use to repersent a link to a list of answers the user can view if need help.
-function Question({question, currentLocation}){
-	
+class Question extends Component {
+  constructor(props){
+    super(props);
+    this.state= {show:false};
+  }    
+    
+
+  showListOfAnswers = () =>{
+    this.setState({show: true});
+  }
+  
+  hideListOfAnswers = () =>{
+    this.setState({show: false});
+  }  
+    
+  render(){
+      
 	//get the list of questions and answers from the test reducer
-	const listOfQuestions = {question}.question;
+	const listOfQuestions = this.props.question;
 	
 	//get the current count of questions answered from the questionAnswered reducer
-	const location = {currentLocation}.currentLocation.questionAnswered;
-	
-	//determine the current question to be displayed based on the number of questions previously answered. 
-	const currentQuestion = listOfQuestions[location].question;
+	const location = this.props.currentLocation.questionAnswered;
 
-  return(
-    <div className="row text-center">
-	  <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-		<h3>
-	  	  {currentQuestion}
-	  	  <span>
-	  		<i className= {`fas fa-book ${css(styles.listOfAnswers)}`}></i>
-		  </span>
-		</h3>
-	  </div>		
-	</div>  
-  );	
+	//determine the current question to be displayed based on the number of questions previously answered. 
+    const currentQuestion = listOfQuestions[location].question; 
+      
+    return(
+      <div className="row text-center">
+	    <div className="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+		  <h3>
+	  	    {currentQuestion}
+            <Button>
+	  	    <span onClick={this.showListOfAnswers}>
+	  		  <i className= {`fas fa-book ${css(styles.listOfAnswers)}`}></i>
+		    </span>
+            </Button>
+		  </h3>
+          <Modal show={this.state.show} onHide={this.hideListOfAnswers}>
+            <Modal.Header closeButton>
+              <Modal.Title>List of Answers</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              
+            </Modal.Body>
+            <Modal.Footer>
+            </Modal.Footer>
+          </Modal>
+	    </div>		
+	  </div>  
+    );
+  }
 }
 
 /*Use Redux to get the current list of questions/answers and number of questions answered*/
