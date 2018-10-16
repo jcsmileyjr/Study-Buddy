54e6c5a25989188460eccb03a530b5c717f862a2
JC Smiley <jcsmileyjr@gmail.com>
2018-10-15 19:22:39 -0500

    Score is displayed with multiple decimal points

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -12,13 +12,15 @@ const styles = StyleSheet.create({
 
 //A permanent component at the top of the app. It shows the app's title, current score, and a menu icon. This icon takes the user to the menu screen. 
 function Nav({currentScore}){
+      
+    
   return(
     <div className={`row ${css(styles.navBackground)}`}>
 	  <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
 		Study Buddy
 	  </div>
 	  <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-center">
-		Score {currentScore.score}%
+		Score {Math.round(currentScore.score)}%
 	  </div>
 	  <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 text-right">
 		<i className="fas fa-bars"></i>
