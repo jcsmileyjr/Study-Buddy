e7945d53754e79cb63d18c06d7a1c0b56ef8eb51
JC Smiley <jcsmileyjr@gmail.com>
2018-10-17 18:56:01 -0500

    New user testing data

--- src/Reducers/tests.js
+++ src/Reducers/tests.js
@@ -9,14 +9,9 @@ export default function test(state=arrayOfAnswers, {type, payload}){
 }
 
 const arrayOfAnswers = [
-	{"question":"Scientific study of heredity","answer":"Genetics", "passFail":"asking"},
-	{"question":"Specific characteristic of an individual","answer":"Trait", "passFail":"asking"},
-	{"question":"Offspring of crosses between parents with different traits","answer":"Hybrid", "passFail":"asking"},
-	{"question":"Sequence of DNA that codesfor a protein and that determines a trait; factors that are pased from parent to offspring","answer":"Genes", "passFail":"asking"},
-	{"question":"One of different forms of a gene","answer":"Alleles", "passFail":"asking"},
-	{"question":"Having two identical alleles for a particular gene","answer":"Homozygous", "passFail":"asking"},
-	{"question":"Having two different alleles for a particular gene","answer":"Heterozygous", "passFail":"asking"},
-	{"question":"Physical characteristics of an organism","answer":"Phenotype", "passFail":"asking"},
-	{"question":"Genetic makeup of an organism","answer":"Genotype", "passFail":"asking"},
-	{"question":"Diagram that can be used to predict the genotype and phenotype combination of a genetic across","answer":"Punnett Square", "passFail":"asking"}    
+	{"question":"1 x 5","answer":"5", "passFail":"asking"},
+	{"question":"2 x 5","answer":"10", "passFail":"asking"},
+	{"question":"3 x 5","answer":"15", "passFail":"asking"},
+	{"question":"4 x 5","answer":"20", "passFail":"asking"},
+	{"question":"5 x 5","answer":"25", "passFail":"asking"}     
 ];
